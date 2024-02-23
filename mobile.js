2b49722d6caeca1f4f3d8a0e8841f3a98d8f3d70
Ladan Torkzaban <99889241+ladantork@users.noreply.github.com>
2024-02-23 13:04:17 +0000

    Advance UK mobile number

--- mobile.js
+++ mobile.js
@@ -1,13 +1,4 @@
 function validMobileNumber(mobileNumber) {
-    let endingChars = '';
-    if (mobileNumber.startsWith('07')) {
-        endingChars += mobileNumber.slice(2);
-    } else if (mobileNumber.startsWith('+447')) {
-        endingChars += mobileNumber.slice(4);
-    } else if (mobileNumber.startsWith('00447')) {
-        endingChars += mobileNumber.slice(5);
-    } else {
-        return false;
-    }
-    return endingChars.length === 9 && !isNaN(endingChars);
+    const regex = /^(07|\+447|00447)\d{9}$/;
+    return regex.test(mobileNumber);
 }
